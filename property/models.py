a4203755f14fb5f8e78d81e2d8a278a41651ff8f
codeschris <chrismlolwa42@outlook.com>
2024-07-20 10:05:45 +0300

    feat: connect gcloud to project

--- property/models.py
+++ property/models.py
@@ -1,3 +1,92 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    BUYER = 'Buyer'
+    REALTOR = 'Realtor'
+    USER_TYPE_CHOICES = [
+        (BUYER, 'Buyer'),
+        (REALTOR, 'Realtor'),
+    ]
+    
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, null=False)
+    email_address = models.EmailField(max_length=255, unique=True, null=False)
+    phone_number = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=100, null=False)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, null=False)
+    profile_picture = models.CharField(max_length=255, null=False)
+    created_at = models.DateField(auto_now_add=True, null=False)
+    
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    profile_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+    bio = models.TextField(null=False)
+    address = models.CharField(max_length=255, null=False)
+    preferences = models.TextField(null=False)
+    updated_at = models.DateField(auto_now=True, null=False)
+    
+    def __str__(self):
+        return self.user.name
+
+class Property(models.Model):
+    property_id = models.AutoField(primary_key=True)
+    realtor = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'user_type': 'Realtor'}, null=False)
+    title = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=255, null=False)
+    price = models.FloatField(null=False)
+    location = models.CharField(max_length=255, null=False)
+    property_type = models.CharField(max_length=100, null=False)
+    bedrooms = models.IntegerField(null=False)
+    bathrooms = models.IntegerField(null=False)
+    area = models.FloatField(null=False)
+    listed_at = models.DateField(auto_now_add=True, null=False)
+    
+    def __str__(self):
+        return self.title
+
+class Feedback(models.Model):
+    feedback_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    content = models.TextField(null=False)
+    created_at = models.DateField(auto_now_add=True, null=False)
+    
+    def __str__(self):
+        return f'Feedback by {self.user.name}'
+
+class Message(models.Model):
+    message_id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE, null=False)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE, null=False)
+    content = models.TextField(null=False)
+    sent_at = models.DateField(auto_now_add=True, null=False)
+    
+    def __str__(self):
+        return f'Message from {self.sender.name} to {self.receiver.name}'
+
+class Photo(models.Model):
+    photo_id = models.AutoField(primary_key=True)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, null=False)
+    url = models.TextField(null=False)
+    created_at = models.DateField(auto_now_add=True, null=False)
+    
+    def __str__(self):
+        return f'Photo of {self.property.title}'
+
+class SearchFilter(models.Model):
+    filter_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    location = models.CharField(max_length=150, null=False)
+    price_min = models.FloatField(null=False)
+    price_max = models.FloatField(null=False)
+    property_type = models.CharField(max_length=50, null=False)
+    bedrooms = models.IntegerField(null=False)
+    bathrooms = models.IntegerField(null=False)
+    area_min = models.FloatField(null=False)
+    area_max = models.FloatField(null=False)
+    created_at = models.DateField(auto_now_add=True, null=False)
+    
+    def __str__(self):
+        return f'Search Filter by {self.user.name}'
--- propertysmart_realtor/settings.py
+++ propertysmart_realtor/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_spanner',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'property',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'propertysmart_realtor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_spanner',
+        'PROJECT': 'My First Project',
+        'INSTANCE': 'propertysmart-042',
+        'NAME': 'properties',
     }
 }
 
