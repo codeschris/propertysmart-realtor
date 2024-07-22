93e9b8e30a9d5326b2ef8b7f743825485235813e
codeschris <chrismlolwa42@outlook.com>
2024-07-23 00:13:12 +0300

    start from here

--- property/urls.py
+++ property/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from .views import register, login_view, VerifyTokenView
+from .views import home
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login_view, name='login'),
-    path('verify-token/', VerifyTokenView.as_view(), name='verify-token'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', home, name="homepage")
 ]
